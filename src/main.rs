6a85058d450bdfd90d843fc70bb4f03677ff01eb
MarRue <20143778+gpluscb@users.noreply.github.com>
2023-12-02 12:30:20 +0100

    make setup a little nicer

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,38 @@ use std::path::PathBuf;
 use std::time::SystemTime;
 use strum::{EnumIter, IntoEnumIterator};
 
-mod day;
+mod challenge;
 
 #[derive(Clone, Eq, PartialEq, Debug)]
 struct TestFailure {
-    day: Day,
+    challenge: Challenge,
     input: String,
     expected_output: String,
     actual_output: String,
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, EnumIter)]
-#[repr(u8)]
-enum Day {
-    Day1 = 1,
+enum Challenge {
+    Day1_1,
+    Day1_2,
 }
 
-impl Day {
-    fn from_number(day_number: u8) -> Option<Day> {
-        Day::iter().find(|&day| day as u8 == day_number)
+impl Challenge {
+    fn from_day_part(day_number: u8, part_number: u8) -> Option<Challenge> {
+        Challenge::iter().find(|challenge| challenge.day_part_number() == (day_number, part_number))
     }
 
-    fn run(&self, part: u8, input: &str) -> String {
-        match (self, part) {
-            (Day::Day1, 1) => day::day1::run_part_1(input),
-            (Day::Day1, 2) => day::day1::run_part_2(input),
-            _ => panic!("No matching challenge"),
+    fn day_part_number(&self) -> (u8, u8) {
+        match self {
+            Challenge::Day1_1 => (1, 1),
+            Challenge::Day1_2 => (1, 2),
+        }
+    }
+
+    fn run(&self, input: &str) -> String {
+        match self {
+            Challenge::Day1_1 => challenge::day1::run_part_1(input),
+            Challenge::Day1_2 => challenge::day1::run_part_2(input),
         }
     }
 }
@@ -46,12 +52,13 @@ fn main() {
         .parse()
         .expect("CURRENT_DAY environment variable must be u8");
 
-    let day = Day::from_number(current_day_number).expect("CURRENT_DAY is not a valid day");
-
     let current_part_number: u8 = std::env::var("CURRENT_PART")
         .expect("Provide CURRENT_PART environment variable")
         .parse()
         .expect("CURRENT_PART environment variable must be u8");
+
+    let challenge = Challenge::from_day_part(current_day_number, current_part_number)
+        .expect("Challenge does not exist");
 
     let part_run_dir_path = base_path
         .join(format!("day{current_day_number}"))
@@ -68,14 +75,14 @@ fn main() {
 
     let input = fs::read_to_string(in_file_path).expect("Could not read in file");
 
-    let output = day.run(current_part_number, &input);
+    let output = challenge.run(&input);
 
     fs::write(out_file_path, output).expect("Could not write");
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{Day, TestFailure};
+    use crate::{Challenge, TestFailure};
     use std::fs;
     use std::path::PathBuf;
     use strum::IntoEnumIterator;
@@ -90,53 +97,39 @@ mod test {
             std::env::var("TESTDATA_DIR").expect("Provide TESTDATA_DIR env variable"),
         );
 
-        for day in Day::iter() {
-            let day_number = day as u8;
+        for challenge in Challenge::iter() {
+            let (day_number, part_number) = challenge.day_part_number();
 
-            let day_dir_path = base_path.join(format!("day{day_number}"));
+            let part_dir_path = base_path
+                .join(format!("day{day_number}"))
+                .join(format!("part{part_number}"));
 
-            for part_dir in day_dir_path.read_dir().expect("Missing day test directory") {
-                let part_dir_path = part_dir
-                    .expect("Problem reading files in day test directory")
+            for day_test_dir in part_dir_path
+                .read_dir()
+                .expect("Missing challenge part directory")
+            {
+                let day_test_dir_path = day_test_dir
+                    .expect("Problem reading files in challenge part directory")
                     .path();
 
-                let part_dir_name = part_dir_path
-                    .file_name()
-                    .expect("Weird entry in day test directory")
-                    .to_str()
-                    .expect("Part dir was not unicode");
+                let in_file_path = day_test_dir_path.join("in.txt");
+                let out_file_path = day_test_dir_path.join("out.txt");
 
-                let part_number: u8 = part_dir_name["part".len()..]
-                    .parse()
-                    .expect("Part dir did not follow format");
+                let input = fs::read_to_string(in_file_path).expect("Could not read in file");
+                let expected_output =
+                    fs::read_to_string(out_file_path).expect("Could not read out file");
 
-                for day_test_dir in part_dir_path
-                    .read_dir()
-                    .expect("Missing day part directory")
-                {
-                    let day_test_dir_path = day_test_dir
-                        .expect("Problem reading files in day part directory")
-                        .path();
+                let actual_output = challenge.run(&input);
 
-                    let in_file_path = day_test_dir_path.join("in.txt");
-                    let out_file_path = day_test_dir_path.join("out.txt");
+                if actual_output != expected_output {
+                    let failure = TestFailure {
+                        challenge,
+                        input,
+                        expected_output,
+                        actual_output,
+                    };
 
-                    let input = fs::read_to_string(in_file_path).expect("Could not read in file");
-                    let expected_output =
-                        fs::read_to_string(out_file_path).expect("Could not read out file");
-
-                    let actual_output = day.run(part_number, &input);
-
-                    if actual_output != expected_output {
-                        let failure = TestFailure {
-                            day,
-                            input,
-                            expected_output,
-                            actual_output,
-                        };
-
-                        failures.push(failure);
-                    }
+                    failures.push(failure);
                 }
             }
         }
